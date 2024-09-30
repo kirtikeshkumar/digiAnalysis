71b03783eda2c90e072ca145fa652990afc29098
kirtikeshkumar <kumarkirtikesh@gmail.com>
2024-09-30 18:34:46 +0530

    Added CMakeLists

---
+++ test/Test_Dummy.cpp
@@ -0,0 +1,10 @@
+/*
+**	Filename : Test_Dummy.cpp
+**	2024-09-30
+**	username : rsehgal
+*/
+#include<iostream>
+int main(int argc, char *argv[]){
+std::cout << "hello DigiAnalysis..." << std::endl;
+return 0;
+}
