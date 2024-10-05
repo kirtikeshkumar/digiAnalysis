545b9901781aa645546e0a01afe84674cb442840
kirtikeshkumar <kumarkirtikesh@gmail.com>
2024-10-06 01:34:11 +0530

    Added Environment variables for nSampleBL and smoothBoxSz in CMAKE and called them in waveform class Waveform class defined except for plot and evaluate decay time functions

--- inc/WaveForm.h
+++ inc/WaveForm.h
@@ -1,6 +1,9 @@
 /*
 This File defines how to hande the waveform
 class WaveForm: parent class contains how to handle waveform including plotting
+
+add a function to concatenate 2 waveforms: this will be useful for the PSBar or any other double readout case
+add a function to take average of vecOfWaveForms
 */
 
 #ifndef WaveForm_h
@@ -12,36 +15,57 @@ class WaveForm: parent class contains how to handle waveform including plotting
 #include <memory>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <cmath>
 
-namespace digiAnalysis {
-class WaveForm {
-protected:
-  std::vector<UShort_t> traces;
-  std::vector<UShort_t> tracesSmooth;
-  std::vector<UShort_t> CFDtraces;
-  float meantime;
+namespace digiAnalysis
+{
+  class WaveForm
+  {
+  protected:
+    std::vector<UShort_t> traces;
+    std::vector<UShort_t> tracesSmooth;
+    std::vector<UShort_t> CFDtraces;
+    float meantime;
+    float baseline;
 
-public:
-  WaveForm();
-  WaveForm(
-      TArrayS *arr); // in definition for this, include if smoothing
-                     // and cfd is to be done while storing using another option
-  WaveForm(std::vector<UShort_t> tr);
-  WaveForm(const WaveForm &wf);
-  virtual ~WaveForm();
-  void Plot();
-  std::vector<UShort_t> GetTraces();
-  float GetMeanTime();
+  public:
+    WaveForm();
+    WaveForm(TArrayS *arr);
+    WaveForm(const std::vector<UShort_t> tr);
+    WaveForm(const WaveForm &wf);
+    WaveForm(const WaveForm &wf1, const WaveForm &wf2);                                                  // constructor to concatenate waveforms
+    WaveForm(UShort_t numWaveForm, UShort_t sizeOfWaveForms, const std::vector<WaveForm> vecOfWaveForm); // Constructor to take average of waveforms
 
-  void SetWaveForm(std::vector<UShort_t> tr);
-  void SetSmooth();
-  void SetCFD();
+    virtual ~WaveForm();
 
-  void EvalDecayTime(UShort_t FitStart, UShort_t FitEnd,
-                     UShort_t numDecayConst);
+    std::vector<UShort_t> GetTraces();
+    std::vector<UShort_t> GetTracesSmooth();
+    float GetMeanTime();
+    float GetBaseLine();
 
-  ClassDef(WaveForm, 1);
-};
+    void SetWaveForm(const std::vector<UShort_t> tr);
+    void SetSmooth(UShort_t smoothBoxSz);
+    void SetCFD();
+    void SetMeanTime();
+    void SetMeanTime(const std::vector<UShort_t> tr);
+    void SetMeanTime(const std::vector<UShort_t> tr, UShort_t start, UShort_t stop);
+    void SetBaseLine();
+    void SetBaseLine(TArrayS *arr);
+    void SetBaseLine(const std::vector<UShort_t> tr);
+
+    void Plot();
+    // std::vector<float> EvalDecayTime(UShort_t FitStart, UShort_t FitEnd, UShort_t numDecayConst);
+    Long64_t IntegrateWaveForm();
+    Long64_t IntegrateWaveForm(int startTime, int stopTime);
+    void ConcatenateWaveForms(const WaveForm &wf1, const WaveForm &wf2);
+    void AverageWaveForms(UShort_t numWaveForm, UShort_t sizeOfWaveForms, const std::vector<WaveForm> vecOfWaveForm);
+
+    static UShort_t nSampleBL;   // Number of baseline samples
+    static UShort_t smoothBoxSz; // Size of smoothing box
+
+    //  ClassDef(WaveForm, 1);
+  };
 
 } // namespace digiAnalysis
 #endif /*WaveForm_h*/
--- src/WaveForm.cpp
+++ src/WaveForm.cpp
@@ -1,54 +1,435 @@
 #include "WaveForm.h"
 #include "TMath.h"
 #include <iostream>
+#include <cmath>
+#include <cstdlib> // to get enviroment variable setSmoothSz
 
 using namespace std;
-namespace digiAnalysis {
-WaveForm::WaveForm() { meantime = 0.0; }
-WaveForm::WaveForm(TArrayS *arr) {
-#ifdef SMOOTH
-  UShort_t smoothBoxSz = 4;
-  unsigned int movingSum;
-#endif
-  meantime = 0;
-  float sampleSum = 0;
-  unsigned int size = arr->GetSize();
-  UShort_t firstVal = arr->At(0);
-  for (unsigned int j = 0; j < size; j++) {
-    traces.push_back(firstVal - arr->At(j));
-    meantime = meantime + traces[j] * j;
-    sampleSum = sampleSum + traces[j];
-#ifdef SMOOTH
-    if (j < smoothBoxSz) {
-      tracesSmooth.push_back(0);
-      movingSum = movingSum + traces[j];
-    }
-    movingSum = movingSum + traces[j] - traces[j - smoothBoxSz];
-    tracesSmooth = movingSum / smoothBoxSz;
-#endif
-#ifdef CFD
-#endif
-  }
-  meantime = TMath::Log10(meantime / sampleSum);
-}
-WaveForm::WaveForm(std::vector<UShort_t> tr) {}
-WaveForm::WaveForm(const WaveForm &wf) {
-  meantime = wf.meantime;
-  if (!wf.traces.empty()) {
-  } // copy traces if not empty
-  if (!wf.tracesSmooth.empty()) {
-  } // copy traces smooth if not empty
-  if (!wf.CFDtraces.empty()) {
-  } // copy traces CFD if not empty
-}
-WaveForm::~WaveForm() {}
-void WaveForm::Plot() {}
-std::vector<UShort_t> WaveForm::GetTraces() { return traces; }
-float WaveForm::GetMeanTime() { return meantime; }
+namespace digiAnalysis
+{
+  // UShort_t WaveForm::nSampleBL = 16;
+  const char *env_var_baselinesamples = std::getenv("NUM_SAMPLE_BASELINE");
+  UShort_t WaveForm::nSampleBL = static_cast<UShort_t>(std::stoul(env_var_baselinesamples));
 
-void WaveForm::SetWaveForm(std::vector<UShort_t> tr) {}
-void WaveForm::SetSmooth() {}
-void WaveForm::SetCFD() {}
-void WaveForm::EvalDecayTime(UShort_t FitStart, UShort_t FitEnd,
-                             UShort_t numDecayConst) {}
+#ifdef SMOOTH
+  const char *envVar = std::getenv("SMOOTH_BOX_SIZE");
+  UShort_t WaveForm::smoothBoxSz = static_cast<UShort_t>(std::stoul(envVar));
+#endif
+
+  /*Constructors*/
+  WaveForm::WaveForm() // Default Constructor initializes empty vectors
+  {
+    meantime = 0.0;
+    baseline = 0.0;
+  }
+
+  WaveForm::WaveForm(TArrayS *arr) // CoMPASS saves waveforms as TArrayS
+  {
+#ifdef SMOOTH
+    unsigned int movingSum = 0;
+#endif
+    meantime = 0;
+    baseline = 0;
+    SetBaseLine(arr); // Alwasy Set Base Line First
+    float sampleSum = 0;
+    unsigned int size = arr->GetSize();
+    for (unsigned int j = 0; j < size; j++)
+    {
+      traces.push_back(baseline - arr->At(j));
+      meantime = meantime + traces[j] * j;
+      sampleSum = sampleSum + traces[j];
+#ifdef SMOOTH
+      if (WaveForm::smoothBoxSz == 1)
+      {
+        tracesSmooth.push_back(traces[j]);
+      }
+      else
+      {
+        if (j < WaveForm::smoothBoxSz)
+        {
+          tracesSmooth.push_back(0);
+          movingSum = movingSum + traces[j];
+        }
+        movingSum = movingSum + traces[j] - traces[j - WaveForm::smoothBoxSz];
+        tracesSmooth.push_back(movingSum / WaveForm::smoothBoxSz);
+      }
+#endif
+    }
+    meantime = TMath::Log10(meantime / sampleSum);
+  }
+
+  WaveForm::WaveForm(const std::vector<UShort_t> tr) // copy from other vector
+  {
+    if (!tr.empty())
+    {
+      SetBaseLine(tr);
+      if (std::fabs(baseline - tr[0]) > 100) // check if the copied traces have baseline subtracted
+      {
+        SetWaveForm(tr);
+      }
+      else
+      {
+        traces = tr;
+        SetMeanTime();
+#ifdef SMOOTH
+        SetSmooth(WaveForm::smoothBoxSz);
+#endif
+      }
+    }
+    else
+    {
+      std::cout << "input vector is empty" << std::endl;
+    }
+  }
+
+  WaveForm::WaveForm(const WaveForm &wf) // copy consructor
+  {
+    meantime = wf.meantime;
+    baseline = wf.baseline;
+    if (!wf.traces.empty()) // copy traces if not empty
+    {
+      traces = wf.traces;
+    }
+    if (!wf.tracesSmooth.empty()) // copy traces smooth if not empty
+    {
+      tracesSmooth = wf.tracesSmooth;
+    }
+    if (!wf.CFDtraces.empty()) // copy traces CFD if not empty
+    {
+      CFDtraces = wf.CFDtraces;
+    }
+  }
+
+  WaveForm::WaveForm(const WaveForm &wf1, const WaveForm &wf2)
+  {
+    ConcatenateWaveForms(wf1, wf2);
+  }
+
+  WaveForm::WaveForm(UShort_t numWaveForm, UShort_t sizeOfWaveForms, const std::vector<WaveForm> vecOfWaveForm)
+  {
+    AverageWaveForms(numWaveForm, sizeOfWaveForms, vecOfWaveForm);
+  }
+
+  /*Destructor*/
+  WaveForm::~WaveForm()
+  {
+    /*clear() Removes elements
+    shrink_to_fit() frees unused memory*/
+    /*
+    traces.clear();
+    traces.shrink_to_fit();
+
+    tracesSmooth.clear();
+    tracesSmooth.shrink_to_fit();
+
+    CFDtraces.clear();
+    CFDtraces.shrink_to_fit();
+    */
+  }
+
+  void WaveForm::Plot() {}
+
+  /*Getters*/
+  std::vector<UShort_t> WaveForm::GetTraces() { return traces; }
+  std::vector<UShort_t> WaveForm::GetTracesSmooth() { return tracesSmooth; }
+  float WaveForm::GetMeanTime() { return meantime; }
+  float WaveForm::GetBaseLine() { return baseline; }
+
+  /*Setters*/
+  void WaveForm::SetWaveForm(std::vector<UShort_t> tr)
+  {
+#ifdef SMOOTH
+    unsigned int movingSum = 0;
+#endif
+    if (!tr.empty())
+    {
+      SetBaseLine(tr);
+      meantime = 0;
+      float sampleSum = 0;
+      unsigned int size = tr.size();
+      for (unsigned int j = 0; j < size; j++)
+      {
+        traces.push_back(baseline - tr[j]);
+        meantime = meantime + traces[j] * j;
+        sampleSum = sampleSum + traces[j];
+#ifdef SMOOTH
+        if (WaveForm::smoothBoxSz == 1)
+        {
+          tracesSmooth.push_back(traces[j]);
+        }
+        else
+        {
+          if (j < WaveForm::smoothBoxSz)
+          {
+            tracesSmooth.push_back(0);
+            movingSum = movingSum + traces[j];
+          }
+          movingSum = movingSum + traces[j] - traces[j - WaveForm::smoothBoxSz];
+          tracesSmooth.push_back(movingSum / WaveForm::smoothBoxSz);
+        }
+#endif
+      }
+      meantime = TMath::Log10(meantime / sampleSum);
+    }
+    else
+    {
+      std::cout << "input vector is empty" << std::endl;
+    }
+  }
+
+  void WaveForm::SetSmooth(UShort_t smoothBoxSz)
+  {
+    if (!traces.empty())
+    {
+      unsigned int movingSum = 0;
+      SetBaseLine();
+      unsigned int size = traces.size();
+      if (smoothBoxSz == 1)
+      {
+        tracesSmooth = traces;
+      }
+      else
+      {
+        for (unsigned int j = 0; j < size; j++)
+        {
+          if (j < smoothBoxSz)
+          {
+            tracesSmooth.push_back(0);
+            movingSum = movingSum + traces[j];
+          }
+          movingSum = movingSum + traces[j] - traces[j - smoothBoxSz];
+          tracesSmooth.push_back(movingSum / smoothBoxSz);
+        }
+      }
+    }
+    else
+    {
+      std::cout << "Fill traces first" << std::endl;
+    }
+  }
+
+  void WaveForm::SetCFD() {}
+
+  void WaveForm::SetMeanTime()
+  {
+    meantime = 0;
+    float sampleSum = 0;
+    if (!traces.empty())
+    {
+      unsigned int size = traces.size();
+      for (unsigned int j = 0; j < size; j++)
+      {
+        meantime = meantime + traces[j] * j;
+        sampleSum = sampleSum + traces[j];
+      }
+      meantime = TMath::Log10(meantime / sampleSum);
+    }
+    else
+    {
+      std::cout << "traces is empty" << std::endl;
+    }
+  }
+
+  void WaveForm::SetMeanTime(const std::vector<UShort_t> tr)
+  {
+    meantime = 0;
+    float sampleSum = 0;
+    if (!tr.empty())
+    {
+      unsigned int size = tr.size();
+      for (unsigned int j = 0; j < size; j++)
+      {
+        meantime = meantime + tr[j] * j;
+        sampleSum = sampleSum + tr[j];
+      }
+      meantime = TMath::Log10(meantime / sampleSum);
+    }
+    else
+    {
+      std::cout << "input vector is empty" << std::endl;
+    }
+  }
+
+  void WaveForm::SetMeanTime(const std::vector<UShort_t> tr, UShort_t start, UShort_t stop)
+  {
+    meantime = 0;
+    float sampleSum = 0;
+    if (!tr.empty())
+    {
+      if (start < stop)
+      {
+        if (tr.size() < stop)
+        {
+          std::cout << "Warning: stop is less than input vector size. Evaluating till end" << std::endl;
+          stop = tr.size();
+        }
+        for (unsigned int j = start; j < stop; j++)
+        {
+          meantime = meantime + tr[j] * j;
+          sampleSum = sampleSum + tr[j];
+        }
+        meantime = TMath::Log10(meantime / sampleSum);
+      }
+      else
+      {
+        std::cout << "error: start > stop, exiting witout meantime setting" << std::endl;
+      }
+    }
+    else
+    {
+      std::cout << "input vector is empty" << std::endl;
+    }
+  }
+
+  void WaveForm::SetBaseLine()
+  {
+    baseline = 0;
+    float sum = 0;
+    if (!traces.empty())
+    {
+      for (unsigned int j = 0; j < WaveForm::nSampleBL; j++)
+      {
+        sum = sum + traces[j];
+      }
+      baseline = sum / WaveForm::nSampleBL;
+    }
+    else
+    {
+      std::cout << "traces is empty" << std::endl;
+    }
+  }
+
+  void WaveForm::SetBaseLine(std::vector<UShort_t> tr)
+  {
+    baseline = 0;
+    float sum = 0;
+    if (!tr.empty())
+    {
+      for (unsigned int j = 0; j < WaveForm::nSampleBL; j++)
+      {
+        sum = sum + tr[j];
+      }
+      baseline = sum / WaveForm::nSampleBL;
+    }
+    else
+    {
+      std::cout << "input vector is empty" << std::endl;
+    }
+  }
+
+  void WaveForm::SetBaseLine(TArrayS *arr)
+  {
+    baseline = 0;
+    float sum = 0;
+    if (arr && (arr->GetSize() > WaveForm::nSampleBL))
+    {
+      for (unsigned int j = 0; j < WaveForm::nSampleBL; j++)
+      {
+        sum = sum + arr->At(j);
+      }
+      baseline = sum / WaveForm::nSampleBL;
+    }
+    else
+    {
+      std::cout << "input array is empty or smaller than NUM_SAMPLE_BASELINE" << std::endl;
+    }
+  }
+
+  // std::vector<float> WaveForm::EvalDecayTime(UShort_t FitStart, UShort_t FitEnd, UShort_t numDecayConst) {}
+  Long64_t WaveForm::IntegrateWaveForm()
+  {
+    Long64_t sum = 0;
+    if (!traces.empty())
+    {
+      sum = IntegrateWaveForm(0, traces.size());
+    }
+    else
+    {
+      std::cout << "fill traces first" << std::endl;
+    }
+    return sum;
+  }
+
+  Long64_t WaveForm::IntegrateWaveForm(int startTime, int stopTime)
+  {
+    Long64_t sum = 0;
+    if (!traces.empty() && (traces.size() > stopTime))
+    {
+      if (startTime < stopTime)
+      {
+        for (unsigned int j = startTime; j < stopTime; j++)
+        {
+          sum = sum + traces[j];
+        }
+      }
+      else
+      {
+        std::cout << "order the times properly " << std::endl;
+      }
+    }
+    else
+    {
+      std::cout << "fill traces first" << std::endl;
+    }
+    return sum;
+  }
+
+  void WaveForm::ConcatenateWaveForms(const WaveForm &wf1, const WaveForm &wf2)
+  {
+    if (!wf1.traces.empty()) // copy traces if not empty
+    {
+      traces = wf1.traces;
+    }
+    if (!wf1.tracesSmooth.empty()) // copy traces smooth if not empty
+    {
+      tracesSmooth = wf1.tracesSmooth;
+    }
+    if (!wf1.CFDtraces.empty()) // copy traces CFD if not empty
+    {
+      CFDtraces = wf1.CFDtraces;
+    }
+
+    if (!wf2.traces.empty()) // concatenate traces if not empty
+    {
+      traces.insert(traces.end(), wf2.traces.begin(), wf2.traces.end());
+    }
+    if (!wf2.tracesSmooth.empty()) // concatenate traces smooth if not empty
+    {
+      tracesSmooth.insert(tracesSmooth.end(), wf2.tracesSmooth.begin(), wf2.tracesSmooth.end());
+    }
+    if (!wf2.CFDtraces.empty()) // concatenate traces CFD if not empty
+    {
+      CFDtraces.insert(CFDtraces.end(), wf2.CFDtraces.begin(), wf2.CFDtraces.end());
+    }
+
+    meantime = traces.size() + 1000; // can use this to check if the waveform is single or concatenated
+    baseline = wf1.baseline;
+  }
+
+  void WaveForm::AverageWaveForms(UShort_t numWaveForm, UShort_t sizeOfWaveForms, const std::vector<WaveForm> vecOfWaveForm)
+  {
+    // currently only traces is averaged
+    UShort_t sum = 0;
+    baseline = 0;
+    for (unsigned int i = 0; i < WaveForm::nSampleBL; i++)
+    {
+      for (unsigned int j = 0; j < numWaveForm; j++)
+      {
+        sum = sum + vecOfWaveForm[j].traces[i] / numWaveForm;
+      }
+      baseline = sum / WaveForm::nSampleBL;
+    }
+
+    for (unsigned int i = 0; i < sizeOfWaveForms; i++)
+    {
+      sum = 0;
+      for (unsigned int j = 0; j < numWaveForm; j++)
+      {
+        sum = sum + vecOfWaveForm[j].traces[i];
+      }
+      sum = baseline - sum / numWaveForm;
+      traces.push_back(sum);
+    }
+    SetMeanTime();
+  }
 } // namespace digiAnalysis
